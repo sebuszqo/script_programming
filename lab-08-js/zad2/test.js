8eae1013138c566e3527319e6a6319c39c14304b
sebuszqo <sekn.kn@gmail.com>
2022-12-06 11:24:22 +0100

    Added Mocha tests

--- lab-08-js/zad2/test.js
+++ lab-08-js/zad2/test.js
@@ -17,34 +17,87 @@
 //     });
 // });
 
-let arrToSum = []
-let sum = 0
-
+// let arrToSum = []
 const addDigits = (string) => {
+
     let arr = string.split("")
     return arr.filter(char => Boolean(Number(char))).reduce((prev, curr)=> Number(prev) + Number(curr), 0)
 }
 
 const countLetters = (string) =>{
-    let arr = string.split("");
-    return arr.filter(char => !Boolean(Number(char))).length
-    // arr = arr.filter(char => !Boolean(Number(char)))
+    const arr = string.split("")
+    return arr.filter(item => isNaN(item)).length
+    // console.log(arr)
 }
+
+let sum = 0
 
 const addAllNumbers = (string) =>{
     let arr = string.split("");
-    return (Number(arr[0]) ? sum += Number(arr.filter(char => Boolean(Number(char))).join("")) : sum )
+    console.log('suma:',sum)
+    return (!isNaN(arr[0]) ? sum += Number(arr.filter(char => !isNaN(char)).join("")) : sum )
+
     // if (Number(arr[0])){
     //     arr = arr.filter(char => Boolean(Number(char)))
     //     return sum += Number(arr.join(""))
     // }
     // return sum
-
 }
 
-console.log(addAllNumbers('111'))
-console.log(addAllNumbers('11aa'))
-console.log(addAllNumbers('b3345b'))
 
-// console.log(addDigits('111'))
-// console.log(countLetters('aAA2s2'))
+"use strict";
+
+let expect = chai.expect;
+describe("The addDigits() function", function () {
+    it("Returns 3 for 111", function () {
+        expect(addDigits("111")).to.equal(3);
+    });
+    it("Returns 0 for aa", function () {
+        expect(addDigits("aa")).to.equal(0);
+    });
+    it("Returns 2 for aa11", function () {
+        expect(addDigits("aa11")).to.equal(2);
+    });
+    it("Returns 2 for 20aab", function () {
+        expect(addDigits("20aab")).to.equal(2);
+    });
+    it("Returns 0 for ''", function () {
+        expect(addDigits("")).to.equal(0);
+    });
+});
+
+describe("The countLetters() function", function () {
+    it("Returns 0 for 111", function () {
+        expect(countLetters("111")).to.equal(0);
+    });
+    it("Returns 2 for aa", function () {
+        expect(countLetters("aa")).to.equal(2);
+    });
+    it("Returns 2 for aa11", function () {
+        expect(countLetters("aa11")).to.equal(2);
+    });
+    it("Returns 3 for 20aab", function () {
+        expect(countLetters("20aab")).to.equal(3);
+    });
+    it("Returns 0 for ''", function () {
+        expect(countLetters("")).to.equal(0);
+    });
+});
+
+describe("The addAllNumbers() function", function () {
+    it("Returns 111 for 111", function () {
+        expect(addAllNumbers("111")).to.equal(111);
+    });
+    it("Returns 111 for aa", function () {
+        expect(addAllNumbers("aa")).to.equal(111);
+    });
+    it("Returns 111 for aa11", function () {
+        expect(addAllNumbers("aa11")).to.equal(111);
+    });
+    it("Returns 131 for 20aab", function () {
+        expect(addAllNumbers("20aab")).to.equal(131);
+    });
+    it("Returns 131 for ''", function () {
+        expect(addAllNumbers("")).to.equal(131);
+    });
+});
